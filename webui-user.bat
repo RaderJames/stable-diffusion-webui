171d702928e19854fee970baa74def4af70facee
RaderJames <122507269+RaderJames@users.noreply.github.com>
2025-02-28 13:09:41 +0800

    Revert "Update webui-user.bat"
    This reverts commit e8fc81b12be654b255ea421dda8cc20dc69e180a.

--- webui-user.bat
+++ webui-user.bat
@@ -1,7 +1,7 @@
 @echo off
 
-set PYTHON=C:\Users\Administrator\AppData\Local\Programs\Python\Python310\python.exe
-set GIT=C:\Users\Administrator\AppData\Local\GitHubDesktop\GitHubDesktop.exe
+set PYTHON=
+set GIT=
 set VENV_DIR=
 set COMMANDLINE_ARGS=
 
