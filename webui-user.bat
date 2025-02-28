ebc51987dd8004c47d3d69b5084d2552250808e2
RaderJames <122507269+RaderJames@users.noreply.github.com>
2025-02-28 13:21:40 +0800

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,7 +1,7 @@
 @echo off
 
-set PYTHON=
-set GIT=
+set PYTHON=C:\Users\Administrator\AppData\Local\Programs\Python\Python310\python.exe
+set GIT=C:\Users\Administrator\AppData\Local\GitHubDesktop\GitHubDesktop.exe
 set VENV_DIR=
 set COMMANDLINE_ARGS=
 
